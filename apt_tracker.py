bfdd6b29687e6b4d2bd1f184d9e73cd155c7505b
CHOIKANGSUK <godche@naver.com>
2026-06-06 13:44:56 +0900

    Add files via upload

--- apt_tracker.py
+++ apt_tracker.py
@@ -1,31 +1,27 @@
 import requests
 import pandas as pd
 import xml.etree.ElementTree as ET
-from datetime import datetime
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import time
 
 API_KEY = "ff4474946de928765829a43d472cc18fbf5fd438b70cbdaf11c7768e5043c961"
 
-# 실시간 자동화 모드: 실행 시점의 당월(YYYYMM)
-target_month = datetime.now().strftime("%Y%m")
+# 2025년 1월 ~ 2026년 5월까지 과거 달력
+months_list = [
+    "202501", "202502", "202503", "202504", "202505", "202506",
+    "202507", "202508", "202509", "202510", "202511", "202512",
+    "202601", "202602", "202603", "202604", "202605"
+]
 
-# 최종 완성된 타겟 리스트 (월계동 삭제, 에코빌/쌍용 추가 완료)
 target_list = [
-    {"lawd_cd": "11260", "dong": "중화동", "keyword": "한신"},
-    {"lawd_cd": "11260", "dong": "상봉동", "keyword": "더샵"},
-    {"lawd_cd": "11230", "dong": "이문동", "keyword": "현대"},
     {"lawd_cd": "11290", "dong": "상월곡동", "keyword": "동아에코빌"},
     {"lawd_cd": "11230", "dong": "이문동", "keyword": "쌍용"}
 ]
 
 def get_apt_transactions(lawd_cd, deal_ymd):
     url = "https://apis.data.go.kr/1613000/RTMSDataSvcAptTradeDev/getRTMSDataSvcAptTradeDev"
-    params = {
-        "serviceKey": API_KEY, "LAWD_CD": lawd_cd, "DEAL_YMD": deal_ymd,
-        "numOfRows": "1000", "pageNo": "1"
-    }
+    params = {"serviceKey": API_KEY, "LAWD_CD": lawd_cd, "DEAL_YMD": deal_ymd, "numOfRows": "1000", "pageNo": "1"}
     try:
         response = requests.get(url, params=params)
         if response.status_code != 200: return []
@@ -50,30 +46,19 @@ def get_apt_transactions(lawd_cd, deal_ymd):
         return parsed_data
     except Exception: return []
 
-print(f"--- {target_month} 기준 최신 실거래가 자동 수집 ---")
-all_filtered_data = []
-unique_lawd_cds = set([target['lawd_cd'] for target in target_list])
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scope)
+gc = gspread.authorize(creds)
+worksheet = gc.open("도권_아파트_실거래가_트래킹").get_worksheet(0)
 
-for cd in unique_lawd_cds:
-    raw_data = get_apt_transactions(cd, target_month)
+all_rows_to_append = []
+for month in months_list:
     for target in target_list:
-        if target['lawd_cd'] == cd:
-            for item in raw_data:
-                if target['dong'] in item['법정동'] and target['keyword'] in item['아파트명']:
-                    all_filtered_data.append(item)
-    time.sleep(0.5)
+        raw_data = get_apt_transactions(target['lawd_cd'], month)
+        for item in raw_data:
+            if target['dong'] in item['법정동'] and target['keyword'] in item['아파트명']:
+                all_rows_to_append.append([item['법정동'], item['아파트명'], item['전용면적(㎡)'], item['층'], item['거래금액(만)'], item['거래일자']])
+        time.sleep(0.5)
 
-if all_filtered_data:
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("secret.json", scope)
-    gc = gspread.authorize(creds)
-    worksheet = gc.open("도권_아파트_실거래가_트래킹").get_worksheet(0)
-    
-    rows_to_append = []
-    for data in all_filtered_data:
-        rows_to_append.append([data['법정동'], data['아파트명'], data['전용면적(㎡)'], data['층'], data['거래금액(만)'], data['거래일자']])
-        
-    worksheet.append_rows(rows_to_append)
-    print(f"\n[🎉] 자동화 전송 완료: {len(rows_to_append)}건 추가됨!")
-else:
-    print("\n[i] 오늘 기준 추가된 새로운 신고 내역이 없습니다.")
+if all_rows_to_append:
+    worksheet.append_rows(all_rows_to_append)
